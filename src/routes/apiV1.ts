9cccb2d8c4701a45711eb13c72b0f15b15a84f45
rejacopyright <reja.copyright@gmail.com>
2024-11-20 12:19:29 +0700

    feat: User Relationship

--- src/routes/apiV1.ts
+++ src/routes/apiV1.ts
@@ -1,9 +1,30 @@
+import { PrismaClient } from '@prisma/client'
 import express from 'express'
 const router = express.Router()
 
-router.get('/me', (req: any, res: any) => {
+const prisma = new PrismaClient({
+  omit: {
+    user: {
+      password: true,
+    },
+  },
+})
+
+router.get('/me', async (req: any, res: any) => {
   const { user } = req
-  return res.status(200).json(user)
+  const data = await prisma.user.findFirst({
+    where: {
+      id: user?.id || '',
+    },
+    include: {
+      religion: true,
+      occupation: true,
+      province: true,
+      city: true,
+    },
+  })
+  console.log(user?.id, data)
+  return res.status(200).json(data)
 })
 
 export default router
