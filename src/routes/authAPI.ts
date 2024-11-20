189caba9f99c9093494f3ab99959e81db7adf575
rejacopyright <reja.copyright@gmail.com>
2024-11-20 16:19:12 +0700

    feat: JWT & Validator

--- src/routes/authAPI.ts
+++ src/routes/authAPI.ts
@@ -1,11 +1,24 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 import express from 'express'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import omit from 'lodash/omit'
+import keyBy from 'lodash/keyBy'
+import mapValues from 'lodash/mapValues'
+import { z } from 'zod'
 
 const router = express.Router()
+
+export const UserValidator = z.object({
+  username: z.string({ required_error: 'Username is required' }).max(100),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .max(100)
+    .email('Email format is not valid'),
+  phone: z.string({ required_error: 'Phone is required' }).max(100),
+  password: z.string({ required_error: 'Password is required' }).max(100),
+})
 
 const prisma = new PrismaClient({
   omit: {
@@ -14,6 +27,19 @@ const prisma = new PrismaClient({
       // created_at: true,
       // updated_at: true,
       // deleted: true,
+    },
+  },
+}).$extends({
+  query: {
+    user: {
+      create({ args, query }: any) {
+        args.data = UserValidator.parse(args.data)
+        return query(args)
+      },
+      update({ args, query }) {
+        args.data = UserValidator.partial().parse(args.data)
+        return query(args)
+      },
     },
   },
 })
@@ -46,10 +72,30 @@ router.post('/login', async (req, res: any) => {
   if (!isPasswordMatch) {
     return res.status(400).json({ message: "Password doesn't match with your account" })
   }
-  currrentUser = omit(currrentUser, ['password', 'deleted'])
+  currrentUser = omit(currrentUser, ['password', 'deleted']) as any
   dotenv.config()
   const token = jwt.sign({ user: currrentUser }, process.env.TOKEN_SECRET, { expiresIn: '1h' })
-  return res.status(200).json({ token, user: currrentUser })
+  const { exp } = jwt.verify(token, process.env.TOKEN_SECRET)
+  return res.status(200).json({ token, exp, user: currrentUser })
+})
+
+router.post('/register', async (req, res: any) => {
+  const { username, password, email, phone } = req?.body
+  const anyUser = await prisma.user.findFirst({ where: { OR: [{ username, email }] } })
+  if (anyUser) {
+    return res.status(400).json({ status: 'failed', message: 'Username or email has been taken' })
+  }
+
+  try {
+    await prisma.user.create({
+      data: { username, email, phone, password: await bcrypt.hash(password, 10) },
+    })
+  } catch (err: any) {
+    const keyByErrors = keyBy(err?.errors, 'path.0')
+    const errors = mapValues(keyByErrors, 'message')
+    return res.status(400).json({ status: 'failed', message: errors })
+  }
+  return res.status(200).json({ status: 'success', message: 'Daftar Berhasil' })
 })
 
 export default router
