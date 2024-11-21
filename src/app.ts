b56e3fc7203cb04e66dd8cd840fb3767d47d23aa
rejacopyright <reja.copyright@gmail.com>
2024-11-21 16:19:16 +0700

    chore: Cors

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import cors, { CorsOptions } from 'cors'
 
 import indexRouter from './routes/index'
 import usersRouter from './routes/users'
@@ -13,6 +14,8 @@ import AuthMiddleWare from './middleware/auth'
 
 const app = express()
 
+// const corsOptions: CorsOptions = { origin: true, credentials: true, allowedHeaders: '*' }
+
 // view engine setup
 app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'jade')
@@ -21,6 +24,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(cors())
 
 app.use('/static', express.static(path.join(__dirname, '../public')))
 
