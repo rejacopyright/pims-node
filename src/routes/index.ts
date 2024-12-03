b2bd62fca688de8e3639eb718e385f572eab2c3d
rejacopyright <reja.copyright@gmail.com>
2024-12-03 21:08:08 +0700

    test: ci1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ const router = express.Router()
 const prisma = new PrismaClient()
 
 router.get('/', async (req, res: any, next) => {
-  return res.status(200).json({ oke: 'oke' })
+  return res.status(200).json({ oke: 'okelah' })
 })
 
 router.get('/city', async (req, res: any, next) => {
