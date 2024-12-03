ff31e69e2849386a3172648877c59086c2e4ae88
rejacopyright <reja.copyright@gmail.com>
2024-12-03 20:46:18 +0700

    chore: pm2

---
+++ pm2.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: 'pims-node',
+      exec_mode: 'cluster',
+      watch: ['src'],
+      // Delay between restart
+      watch_delay: 1000,
+      ignore_watch: ['node_modules', '\\.git', '*.log'],
+      instances: 1, // Or a number of instances
+      script: 'ts-node',
+      args: './src/bin/www.ts',
+      env_local: {
+        APP_ENV: 'local', // APP_ENV=local
+      },
+      env_dev: {
+        APP_ENV: 'dev', // APP_ENV=dev
+      },
+      env_prod: {
+        APP_ENV: 'prod', // APP_ENV=prod
+        PORT: 4000,
+      },
+    },
+  ],
+}
