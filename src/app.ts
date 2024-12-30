d1be2606d9495fa92b50bb3d8e2272cd20c05e92
rejacopyright <reja.copyright@gmail.com>
2024-12-30 16:20:05 +0700

    fix: https getServer

--- src/app.ts
+++ src/app.ts
@@ -37,6 +37,7 @@ app.set('views', path.join(__dirname, '../views'))
 app.engine('html', renderFile)
 app.set('view engine', 'html')
 
+app.enable('trust proxy')
 app.use(logger('dev'))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: false }))
